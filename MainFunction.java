27514e8a38b6ccf508b551c85cdd129adbe66cbd
leeinscky <1009694687@qq.com>
2020-04-29 11:21:11 +0800

    first commit

---
+++ MainFunction.java
@@ -0,0 +1,9 @@
+public class MainFunction {
+
+   public static void main (String[] args) {
+       Integer[] intarray = new Integer[] {2,13,4,5,2,5,6,1,9};
+       //ArrayList<Integer> intarray = new ArrayList<Integer> (2,13,4,5,2,5,6,1,9);
+       Selection sel = new Selection();
+       sel.sort(intarray);
+   }
+}
