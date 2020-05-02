e8475e168e02591f580e9c683ac3e8d5d4cb6a8f
leeinscky <1009694687@qq.com>
2020-05-02 15:27:39 +0800

    update

--- 排序算法/HeapSort.java
+++ 排序算法/HeapSort.java
@@ -16,9 +16,10 @@ public class HeapSort {
         int length = arr.length;
         buildHeap(arr, length);// 原始序列构成的堆是无序的，根据给定的原始序列构建一个大顶堆，上面的节点值比下面的大
         System.out.println("成功构建堆以后的数组 arr:" + Arrays.toString(arr));
+        System.out.println(" ");
 
         for ( int i = length - 1; i > 0; i-- ) {
-            System.out.println("i:" + i);
+            System.out.println("数组长度 i = " + i);
             System.out.println("元素下沉前的初始数组 arr:" + Arrays.toString(arr));
 
             //将堆顶元素与末位元素调换
@@ -27,8 +28,9 @@ public class HeapSort {
             arr[i] = temp;
             length--; // 数组长度-1 隐藏堆尾元素
             System.out.println("开始执行 sort 函数里的sink方法");
-            sink(arr, 0, length); // 堆顶元素和末位元素互换以后的堆从有序变为了无序。因此要继续调用sink()函数将堆顶元素下沉 目的是将最大的元素浮到堆顶来
-
+            System.out.println("元素下沉前 堆顶堆尾互换后的数组 arr:" + Arrays.toString(arr));
+            sink(arr, 0, length); // 堆顶元素和末位元素互换以后的堆从有序变为了无序。通过继续调用sink()函数将长度减少1的新数组进行排序构成大顶堆（父节点都比孩子节点的值大）由于堆要时刻保持一个规则特性：每个子节点的值都比父节点小，所以一旦堆里面的数据发生变化，我们必须对堆重新进行一次构建（通过sink函数重新构建）
+            //sink函数使用0参数的原因：重新构建大顶堆的时候我们从上（顶）至下（底）开始构建
             System.out.println("此次i循环-元素下沉后的数组 arr:" + Arrays.toString(arr));
             System.out.println(" ");
 
@@ -72,17 +74,21 @@ public class HeapSort {
             present = rightChild;
         }
 
-        System.out.println(" present:" + present);
+        System.out.print("    present:" + present);
 
-        if (present != index) {// 如果下标不相等 证明调换过了
-            //交换值
+        if (present != index) {// 如果下标不相等 证明下标调换过了，但是我们还没有调换下标对应的值，所以我们调换 arr[index] 和 arr[present]
+            //交换值 交换arr[index] 和 arr[present]
             int temp = arr[index];
             arr[index] = arr[present];
             arr[present] = temp;
 
+            System.out.println(" 更新后的arr:" + Arrays.toString(arr));
+
             //继续下沉
             sink(arr, present, length);
+
         }
+
 
     }
 
@@ -95,3 +101,61 @@ public class HeapSort {
 
 }
 
+/*
+开始构建堆- 执行buildHeap函数里的sink方法
+index:3   leftChild:7   rightChild:8    present:3 
+构建堆过程中- 此次i循环以后的数组 arr:[8, 2, 5, 9, 7, 3]
+ 
+开始构建堆- 执行buildHeap函数里的sink方法
+index:2   leftChild:5   rightChild:6    present:2
+构建堆过程中- 此次i循环以后的数组 arr:[8, 2, 5, 9, 7, 3]
+ 
+开始构建堆- 执行buildHeap函数里的sink方法
+index:1   leftChild:3   rightChild:4    present:3 更新后的arr:[8, 9, 5, 2, 7, 3]
+index:3   leftChild:7   rightChild:8    present:3
+构建堆过程中- 此次i循环以后的数组 arr:[8, 9, 5, 2, 7, 3]
+ 
+开始构建堆- 执行buildHeap函数里的sink方法
+index:0   leftChild:1   rightChild:2    present:1 更新后的arr:[9, 8, 5, 2, 7, 3]
+index:1   leftChild:3   rightChild:4    present:1
+构建堆过程中- 此次i循环以后的数组 arr:[9, 8, 5, 2, 7, 3]
+ 
+成功构建堆以后的数组 arr:[9, 8, 5, 2, 7, 3]
+ 
+数组长度 i = 5
+元素下沉前的初始数组 arr:[9, 8, 5, 2, 7, 3]
+开始执行 sort 函数里的sink方法
+元素下沉前 堆顶堆尾互换后的数组 arr:[3, 8, 5, 2, 7, 9]
+index:0   leftChild:1   rightChild:2    present:1 更新后的arr:[8, 3, 5, 2, 7, 9]
+index:1   leftChild:3   rightChild:4    present:4 更新后的arr:[8, 7, 5, 2, 3, 9]
+index:4   leftChild:9   rightChild:10    present:4此次i循环-元素下沉后的数组 arr:[8, 7, 5, 2, 3, 9]
+ 
+数组长度 i = 4
+元素下沉前的初始数组 arr:[8, 7, 5, 2, 3, 9]
+开始执行 sort 函数里的sink方法
+元素下沉前 堆顶堆尾互换后的数组 arr:[3, 7, 5, 2, 8, 9]
+index:0   leftChild:1   rightChild:2    present:1 更新后的arr:[7, 3, 5, 2, 8, 9]
+index:1   leftChild:3   rightChild:4    present:1此次i循环-元素下沉后的数组 arr:[7, 3, 5, 2, 8, 9]
+ 
+数组长度 i = 3
+元素下沉前的初始数组 arr:[7, 3, 5, 2, 8, 9]
+开始执行 sort 函数里的sink方法
+元素下沉前 堆顶堆尾互换后的数组 arr:[2, 3, 5, 7, 8, 9]
+index:0   leftChild:1   rightChild:2    present:2 更新后的arr:[5, 3, 2, 7, 8, 9]
+index:2   leftChild:5   rightChild:6    present:2此次i循环-元素下沉后的数组 arr:[5, 3, 2, 7, 8, 9]
+ 
+数组长度 i = 2
+元素下沉前的初始数组 arr:[5, 3, 2, 7, 8, 9]
+开始执行 sort 函数里的sink方法
+元素下沉前 堆顶堆尾互换后的数组 arr:[2, 3, 5, 7, 8, 9]
+index:0   leftChild:1   rightChild:2    present:1 更新后的arr:[3, 2, 5, 7, 8, 9]
+index:1   leftChild:3   rightChild:4    present:1此次i循环-元素下沉后的数组 arr:[3, 2, 5, 7, 8, 9]
+ 
+数组长度 i = 1
+元素下沉前的初始数组 arr:[3, 2, 5, 7, 8, 9]
+开始执行 sort 函数里的sink方法
+元素下沉前 堆顶堆尾互换后的数组 arr:[2, 3, 5, 7, 8, 9]
+index:0   leftChild:1   rightChild:2    present:0此次i循环-元素下沉后的数组 arr:[2, 3, 5, 7, 8, 9]
+ 
+[2, 3, 5, 7, 8, 9] 
+*/
